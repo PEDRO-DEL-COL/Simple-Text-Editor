07d45ed17cc414ce31907eebac69c54ce6178e09
Pedro <pedroavgdelcol@gmail.com>
2024-05-17 18:49:36 -0300

    V1 text editor - index and style added with responsive features

---
+++ script.js
@@ -0,0 +1,24 @@
+let optionButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option.button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("font-size");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "Cursive",
+];
+
+const intializer = () => {
+    
+};
